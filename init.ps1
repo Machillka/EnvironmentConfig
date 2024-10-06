e2c7b4bb7d15f6fe6f5aea7ebccbf340fe7a5eaa
Machillka <machillka2007@gmail.com>
2024-10-07 01:04:40 +0800

    first commit

---
+++ init.ps1
@@ -0,0 +1,27 @@
+# 激活 windows
+slmgr -ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr -skms kms.0t.net.cn
+slmgr -ato
+
+# 右键菜单复原
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+# 临时代理 ( 对话级别 )
+$env:HTTP_PROXY='127.0.0.1:2080'
+$env:HTTPS_PROXY='127.0.0.1:2080'
+
+# 配置 scoop
+Set-Executionpolicy remotesigned -Scope currentuser
+
+## 配置安装目录
+$env:scoopLocal='D:\Scoop'
+$env:scoopGlobal='D:\Scoop\Global'
+
+[System.Environment]::SetEnvironmentVariable('SCOOP', $env:scoopLocal, 'User')
+[System.Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:scoopGlobal, 'Machine')
+
+## 安装
+Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+
+## 配置 scoop 代理
+scoop config proxy 127.0.0.1:2080
